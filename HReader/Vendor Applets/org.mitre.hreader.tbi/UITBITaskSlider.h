5033dd919b90186aa3dea8b6a6aa85ace36fb6b0
ssaltzman <ssaltzman@mitre.org>
2012-10-18 10:46:07 -0400

    Updated UITBITaskSlider and added images

--- HReader/Vendor Applets/org.mitre.hreader.tbi/UITBITaskSlider.h
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/UITBITaskSlider.h
@@ -10,4 +10,6 @@
 
 @interface UITBITaskSlider : UISlider
 
+- (void) appearance;
+
 @end
--- HReader/Vendor Applets/org.mitre.hreader.tbi/UITBITaskSlider.m
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/UITBITaskSlider.m
@@ -14,14 +14,29 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setMinimumValueImage:[UIImage imageNamed:@"slider"]];
-        [self setMinimumTrackImage:[UIImage imageNamed:@"slider"] forState:0];
-        [self setThumbImage:[UIImage imageNamed:@"slider"] forState:0];
-        [self setMaximumTrackImage:[UIImage imageNamed:@"slider"] forState:0];
-        [self setMaximumValueImage:[UIImage imageNamed:@"slider"]];
+        [self appearance];
         
     }
     return self;
+}
+
+- (void) appearance
+{
+    //NSLog(@"imageTest: %@ and %@", [UIImage imageNamed:@"slider"], [UIImage imageNamed:@"slider-1px"]);
+    [self setMinimumValueImage:[UIImage imageNamed:@"sliderEnd"]];
+    [self setMinimumTrackImage:[UIImage imageNamed:@"sliderBarGreen"] forState:0];
+    [self setThumbImage:[UIImage imageNamed:@"slider"] forState:0];
+    [self setMaximumTrackImage:[UIImage imageNamed:@"sliderBar"] forState:0];
+    [self setMaximumValueImage:[UIImage imageNamed:@"sliderEnd"]];
+}
+
+- (CGRect)thumbRectForBounds:(CGRect)bounds trackRect:(CGRect)rect value:(float)value{
+    CGSize thumbSize = [self thumbImageForState:0].size;
+    //NSLog(@"bounds: at (%f, %f) with size (%f, %f)",bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height);
+    //NSLog(@"rect:   at (%f, %f) with size (%f, %f)",rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
+    CGRect returnRect = [super thumbRectForBounds:bounds trackRect:rect value:value];
+    //NSLog(@"returns: at (%f, %f) with size (%f, %f)",returnRect.origin.x, returnRect.origin.y, returnRect.size.width, returnRect.size.height);
+    return [super thumbRectForBounds:bounds trackRect:rect value:value];
 }
 
 /*
