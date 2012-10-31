5a706af943a58ec499d54600f742038f9f4dd419
Kaye, Lindsay M <lkaye@mitre.org>
2012-10-31 13:55:04 -0400

    Kind of working uicollectionview for scrolling

---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/TBIUIImageView.h
@@ -0,0 +1,13 @@
+//
+//  TBIUIImageView.h
+//  HReader
+//
+//  Created by Kaye, Lindsay M on 10/22/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBIUIImageView : UIImageView
+
+@end
---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/TBIUIImageView.m
@@ -0,0 +1,31 @@
+//
+//  TBIUIImageView.m
+//  HReader
+//
+//  Created by Kaye, Lindsay M on 10/22/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "TBIUIImageView.h"
+
+@implementation TBIUIImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
