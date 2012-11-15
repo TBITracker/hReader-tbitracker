877d3ea5334b638214848e985c152c5d25ff8ceb
ssaltzman <ssaltzman@mitre.org>
2012-11-15 15:10:02 -0500

    Missing file restored.

---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/TBIUserInputItem.h
@@ -0,0 +1,32 @@
+//
+//  TBIUserInputItem.h
+//  HReader
+//
+//  Created by Saltzman, Shep on 11/15/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class TBIAudio, TBIImage, TBIStep;
+
+@interface TBIUserInputItem : NSManagedObject
+
+@property (nonatomic, retain) NSString * summary;
+@property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) TBIAudio *audio;
+@property (nonatomic, retain) TBIImage *image;
+@property (nonatomic, retain) TBIStep *step;
+
+- (TBIUserInputItem *) initWithText:(NSString *)textInput;
+- (TBIUserInputItem *) initWithText:(NSString *)textInput andSummary:(NSString *)summaryInput;
+- (TBIUserInputItem *) initWithAudio:(id)audioInput;
+- (TBIUserInputItem *) initWithAudio:(id)audioInput andSummary:(NSString *)summaryInput;
+- (TBIUserInputItem *) initWithImage:(UIImage *)imageInput;
+- (TBIUserInputItem *) initWithImage:(UIImage *)imageInput andSummary:(NSString *)summaryInput;
+
+- (id) getItem;
+//- (UIView *) getView;
+
+@end
---
+++ HReader/Vendor Applets/org.mitre.hreader.tbi/TBIUserInputItem.m
@@ -0,0 +1,82 @@
+//
+//  TBIUserInputItem.m
+//  HReader
+//
+//  Created by Saltzman, Shep on 11/15/12.
+//  Copyright (c) 2012 MITRE Corporation. All rights reserved.
+//
+
+#import "TBIUserInputItem.h"
+#import "TBIAudio.h"
+#import "TBIImage.h"
+#import "TBIStep.h"
+
+
+@implementation TBIUserInputItem
+
+@dynamic summary;
+@dynamic text;
+@dynamic audio;
+@dynamic image;
+@dynamic step;
+
+- (void) reset {
+    self.text = nil;
+    self.audio = nil;
+    self.image = nil;
+}
+
+- (TBIUserInputItem *) initWithText:(NSString *)textInput {
+    return [self initWithText:textInput andSummary:@"No summary"];
+}
+
+- (TBIUserInputItem *) initWithText:(NSString *)textInput andSummary:(NSString *)summaryInput{
+    if (self = [super init]){
+        [self reset];
+        self.text = textInput;
+    }
+    return self;
+}
+
+- (TBIUserInputItem *) initWithAudio:(id)audioInput {
+    return [self initWithAudio:audioInput andSummary:@"No summary"];
+}
+
+- (TBIUserInputItem *) initWithAudio:(id)audioInput andSummary:(NSString *)summaryInput {
+    if (self = [super init]){
+        [self reset];
+        self.audio = [[TBIAudio alloc] initWithAudio:audioInput];
+    }
+    return self;
+}
+
+- (TBIUserInputItem *) initWithImage:(UIImage *)imageInput {
+    return [self initWithImage:imageInput andSummary:@"No summary"];
+}
+
+- (TBIUserInputItem *) initWithImage:(UIImage *)imageInput andSummary:(NSString *)summaryInput {
+    if (self = [super init]){
+        [self reset];
+        self.image = [[TBIImage alloc] initWithImage:imageInput];
+    }
+    return self;
+}
+
+- (id) getItem {
+    if (self.text != nil){
+        return self.text;
+    }
+    else if (self.audio != nil){
+        return self.audio;
+    }
+    else if (self.image != nil){
+        return self.image;
+    }
+    return nil;
+}
+
+- (NSString *) description {
+    return [NSString stringWithFormat:@"%@ (%@)", [self getItem], self.summary];
+}
+
+@end
